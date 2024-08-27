d661ffdfce77db2a6997c85d6ae82e6e433c2701
Mohamed Amin Gana <mohamedaming146@gmail.com>
2024-08-28 00:33:33 +0100

    version 27/08/2024 v1

---
+++ src/main/java/com/medical/medical/controller/UIController/SecretaireControle.java
@@ -0,0 +1,10 @@
+package com.medical.medical.controller.UIController;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component("uiSecretaireController")
+@Slf4j
+public class SecretaireControle {
+
+}
